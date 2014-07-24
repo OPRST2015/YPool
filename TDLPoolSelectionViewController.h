cd07ecf9aa308c16fea0482ce436e2b9249bad48
Carl Baltrunas <cbaltrunas@yahoo-inc.com>
2014-07-24 15:40:20 -0700

    Added presentation of v c for passenger view, updated fields Added presentation of v c for selection of a pool

---
+++ TDLPoolSelectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  TLDPoolSelectionViewController.h
+//  YPool
+//
+//  Created by Carl Baltrunas on 7/24/14.
+//  Copyright (c) 2014 Sudip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLDPoolSelectionViewController : UIViewController
+
+@end
---
+++ YPool/TDLPassengerViewController.m
@@ -0,0 +1,38 @@
+//
+//  TDLPassengerViewController.m
+//  YPool
+//
+//  Created by Carl Baltrunas on 7/24/14.
+//  Copyright (c) 2014 Sudip. All rights reserved.
+//
+
+#import "TDLPassengerViewController.h"
+
+@interface TDLPassengerViewController ()
+
+@end
+
+@implementation TDLPassengerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
